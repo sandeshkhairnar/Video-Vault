50f07dfaa1d64fe5e9a0b9bc41e87d5d045521b5
Sandesh Khairnar <sandeshkhairnar123@gmail.com>
2024-12-10 12:22:10 +0530

    update 3

--- backend/app.py
+++ backend/app.py
@@ -86,4 +86,4 @@ def delete_video():
     return jsonify({"message": "Video deleted successfully"}), 200
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run()
